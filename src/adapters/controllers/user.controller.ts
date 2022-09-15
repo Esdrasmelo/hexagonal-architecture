33257355c455cc74324433d04b5246b50c048bee
Esdrasmelo <emelomotta@gmail.com>
2022-09-15 16:53:13 -0300

    feat: add new usecase on controller

--- src/adapters/controllers/user.controller.ts
+++ src/adapters/controllers/user.controller.ts
@@ -1,21 +1,27 @@
 import { UserPayloadIn } from '../../domain/entities/in';
 import { HttpRequest, HttpResponse } from '../../domain/protocols';
-import { CreateUserUseCase } from '../../domain/usecases';
+import { CreateUserUseCase, FindAllUsersUseCase } from '../../domain/usecases';
 
 export class UserController {
   private createUserUseCase: CreateUserUseCase;
+  private findAllUsersUseCase: FindAllUsersUseCase;
 
-  constructor(createUserUseCase: CreateUserUseCase) {
+  constructor(
+    createUserUseCase: CreateUserUseCase,
+    findAllUsersUseCase: FindAllUsersUseCase,
+  ) {
     this.createUserUseCase = createUserUseCase;
+    this.findAllUsersUseCase = findAllUsersUseCase;
   }
 
   async createUser(userData: HttpRequest): Promise<HttpResponse> {
     const body: UserPayloadIn = userData.body;
-    const user = await this.createUserUseCase.execute({
-      email: body.email,
-      password: body.password,
-      name: body.name,
-    });
+    const user = await this.createUserUseCase.execute(body);
     return user;
+  }
+
+  async findUsers(): Promise<HttpResponse> {
+    const users = await this.findAllUsersUseCase.execute();
+    return users;
   }
 }
