9841578ee8de84c35d3fdfcb50939ad77c990d50
Esdrasmelo <emelomotta@gmail.com>
2022-09-16 18:57:31 -0300

    feat: add new method on user controller

--- src/adapters/controllers/user.controller.ts
+++ src/adapters/controllers/user.controller.ts
@@ -1,17 +1,24 @@
 import { UserPayloadIn } from '../../domain/entities/in';
 import { HttpRequest, HttpResponse } from '../../domain/protocols';
-import { CreateUserUseCase, FindAllUsersUseCase } from '../../domain/usecases';
+import {
+  CreateUserUseCase,
+  FindAllUsersUseCase,
+  FindUserByEmailUseCase,
+} from '../../domain/usecases';
 
 export class UserController {
   private createUserUseCase: CreateUserUseCase;
   private findAllUsersUseCase: FindAllUsersUseCase;
+  private findUserByEmailUseCase: FindUserByEmailUseCase;
 
   constructor(
     createUserUseCase: CreateUserUseCase,
     findAllUsersUseCase: FindAllUsersUseCase,
+    findUserByEmailUseCase: FindUserByEmailUseCase,
   ) {
     this.createUserUseCase = createUserUseCase;
     this.findAllUsersUseCase = findAllUsersUseCase;
+    this.findUserByEmailUseCase = findUserByEmailUseCase;
   }
 
   async createUser(userData: HttpRequest): Promise<HttpResponse> {
@@ -23,5 +30,11 @@ export class UserController {
   async findUsers(): Promise<HttpResponse> {
     const users = await this.findAllUsersUseCase.execute();
     return users;
+  }
+
+  async findUserByEmail(userData: HttpRequest): Promise<HttpResponse> {
+    const email: string = userData.body.email;
+    const user = await this.findUserByEmailUseCase.execute(email);
+    return user;
   }
 }
