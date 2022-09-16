1d9507ebdf459a8e954bc742872416430f04b1bc
Esdrasmelo <emelomotta@gmail.com>
2022-09-16 18:57:14 -0300

    feat: add new user api route

--- src/adapters/api/express/routes/user.routes.ts
+++ src/adapters/api/express/routes/user.routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import {
   CreateUserUseCase,
   FindAllUsersUseCase,
+  FindUserByEmailUseCase,
 } from '../../../../domain/usecases';
 import { UserController } from '../../../controllers/user.controller';
 import { UserRepositoryImpl } from '../../../database/repositories';
@@ -11,19 +12,26 @@ const userRouter = Router();
 const userRepository = new UserRepositoryImpl();
 const createUserUseCase = new CreateUserUseCase(userRepository);
 const findAllUsersUseCase = new FindAllUsersUseCase(userRepository);
+const findUserByEmailUseCase = new FindUserByEmailUseCase(userRepository);
 const userController = new UserController(
   createUserUseCase,
   findAllUsersUseCase,
+  findUserByEmailUseCase,
 );
 
 userRouter.route('/users/create').post(async (request, response) => {
-  const { status, body } = await userController.createUser(request);
-  return response.status(status).send(body);
+  const { statusCode, body } = await userController.createUser(request);
+  return response.status(statusCode).send(body);
 });
 
 userRouter.route('/users').get(async (request, response) => {
-  const { status, body } = await userController.findUsers();
-  return response.status(status).send(body);
+  const { statusCode, body } = await userController.findUsers();
+  return response.status(statusCode).send(body);
+});
+
+userRouter.route('/users/findByEmail').get(async (request, response) => {
+  const { statusCode, body } = await userController.findUserByEmail(request);
+  return response.status(statusCode).send(body);
 });
 
 export default userRouter;
